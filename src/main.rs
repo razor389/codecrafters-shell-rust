cee480d23af14e67da7a658d3317140bab507b09
razor389 <rgranowski@gmail.com>
2024-09-25 08:32:56 -0500

    missing commands

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ fn main() {
     // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-    println!("{}: command not found", input);
+    match stdin.read_line(&mut input) {
+        Ok(_n) => println!("{}: command not found", input),
+        Err(error) => println!("error: {error}"),
+    }
 }
